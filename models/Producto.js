f0928424dfa4e169663f48f4f9ccee5a510fae98
Isaac Ramirez <ramirezisaac.1518@gmail.com>
2021-05-25 22:19:11 -0600

    Coneccion a base de datos establecida, modelo para guardar datos y dependencias(mongoose) agregadas

---
+++ models/Producto.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require("mongoose")
+
+
+const ProductoSchema = new Schema({
+    nombre: {
+       type: String, 
+       required: true 
+    },
+    precio: { 
+      type: Number, 
+      required: true 
+    },
+    imagen: {
+       type: String, 
+       required: true 
+    },
+    fecha: { 
+      type: Date, 
+      default: Date.now 
+    }
+  });
+
+module.exports= model("Producto", ProductoSchema)
